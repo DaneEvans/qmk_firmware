3cb4b91295fda14c24f6b43b309ecc78aefbefb4
dane <dane@goneepic.com>
2020-08-12 22:02:28 +1000

    compiles, but no leds. stole a pile from the drashna one

--- keyboards/crkbd/keymaps/devdev/rules.mk
+++ keyboards/crkbd/keymaps/devdev/rules.mk
@@ -5,10 +5,13 @@ SRC +=  ./lib/glcdfont.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
+		./users/drashna \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
 
 
-RGBLIGHT_ENABLE = yes
-#RGB_MATRIX_ENABLE = WS2812
+#RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = WS2812
+
+
