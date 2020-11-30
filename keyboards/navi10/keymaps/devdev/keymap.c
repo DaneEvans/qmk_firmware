4aa36b57a622dbac17fff7f85078eb9c90a40b4a
Dane Evans <dane@goneepic.com>
2020-11-30 17:52:07 +1100

    adding header and forcing recompile

--- keyboards/navi10/keymaps/devdev/keymap.c
+++ keyboards/navi10/keymaps/devdev/keymap.c
@@ -13,6 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ // NAVI 10
 #include QMK_KEYBOARD_H
 
 #define INDICATOR_LED   B5
