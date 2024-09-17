188d47f9f115d74f79f4bee5227d11dc9c7dfea0
Dane Evans <dane@2pisoftware.com>
2024-09-18 00:36:26 +1000

    remove all but dh

--- keyboards/gecko/keymaps/devdev/keymap.c
+++ keyboards/gecko/keymaps/devdev/keymap.c
@@ -19,9 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
-    _DEFAULTS = 0,
-    _QWERTY = 0,
-    _COLEMAK,
+    // _DEFAULTS = 0,
+    // _QWERTY = 0,
+    // _COLEMAK,
 	_COLEMAKDH, 
     _LOWER,
     _RAISE,
@@ -43,17 +43,17 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_split_3x6_5(
-    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-    LT(_NUMPAD,KC_TAB),KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,             LT(_SWITCH,KC_Y),  KC_U,    KC_I,    KC_O,   KC_P,  KC_SMART_BSP,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                    KC_MUTE,   KC_X,  KC_LGUI, MO(_LOWER),     KC_SPC,       KC_ENT,   MO(_RAISE), KC_RALT,  MO(_SWITCH), KC_SLSH
-                                        //`--------------------------'  `--------------------------'
-    ),
+    // [_QWERTY] = LAYOUT_split_3x6_5(
+    // //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+    // LT(_NUMPAD,KC_TAB),KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,             LT(_SWITCH,KC_Y),  KC_U,    KC_I,    KC_O,   KC_P,  KC_SMART_BSP,
+    // //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    //     KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    // //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    //     KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+    // //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+    //                 KC_MUTE,   KC_X,  KC_LGUI, MO(_LOWER),     KC_SPC,       KC_ENT,   MO(_RAISE), KC_RALT,  MO(_SWITCH), KC_SLSH
+    //                                     //`--------------------------'  `--------------------------'
+    // ),
 
     [_COLEMAKDH] = LAYOUT_split_3x6_5( \
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -67,17 +67,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         //`--------------------------'  `--------------------------'
     ),
 
-    [_COLEMAK] = LAYOUT_split_3x6_5( \
-    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        LT(_NUMPAD,KC_TAB), 	KC_Q, 	 KC_W, 	  KC_F,    KC_P,    KC_B, 		LT(_SWITCH,KC_J),    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_SMART_BSP,  \
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TRNS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, 						  KC_H,    KC_N,    KC_E,    KC_I,LT(_NUMPAD,KC_O),KC_QUOT, \
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|		
-        KC_TRNS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, 						  KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,  \
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                        KC_TRNS, KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS,   KC_TRNS,    MO(_RAISE), KC_TRNS, KC_TRNS, KC_TRNS  \
-                                        //`--------------------------'  `--------------------------'
-    ),
+    // [_COLEMAK] = LAYOUT_split_3x6_5( \
+    // //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+    //     LT(_NUMPAD,KC_TAB), 	KC_Q, 	 KC_W, 	  KC_F,    KC_P,    KC_B, 		LT(_SWITCH,KC_J),    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_SMART_BSP,  \
+    // //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    //     KC_TRNS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, 						  KC_H,    KC_N,    KC_E,    KC_I,LT(_NUMPAD,KC_O),KC_QUOT, \
+    // //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|		
+    //     KC_TRNS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, 						  KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,  \
+    // //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+    //                     KC_TRNS, KC_TRNS, KC_TRNS, MO(_LOWER), KC_TRNS,   KC_TRNS,    MO(_RAISE), KC_TRNS, KC_TRNS, KC_TRNS  \
+    //                                     //`--------------------------'  `--------------------------'
+    // ),
 
     [_LOWER] = LAYOUT_split_3x6_5(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -160,7 +160,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 static void render_logo(void) {
     static const char PROGMEM qmk_logo[] = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0x00, 0xA6, 0x00, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
         0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
     };
 
@@ -180,12 +180,12 @@ void oled_render_layer_state(void) {
   char string [24]; 
   switch (get_highest_layer(default_layer_state|layer_state))
   {
-      case _QWERTY:
-        oled_write_ln_P(PSTR("Layer: QWERTY"),false);
-        break;
-      case _COLEMAK:
-        oled_write_ln_P(PSTR("Layer: COLEMAK"),false);
-        break;    
+    //   case _QWERTY:
+    //     oled_write_ln_P(PSTR("Layer: QWERTY"),false);
+    //     break;
+    //   case _COLEMAK:
+    //     oled_write_ln_P(PSTR("Layer: COLEMAK"),false);
+    //     break;    
       case _COLEMAKDH:
         oled_write_ln_P(PSTR("Layer: COLEMAKDH"),false);
         break;    
@@ -220,16 +220,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     //uint16_t       temp_keycode = keycode;
 	
     switch (keycode) {
-        case KC_QWERTY:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return false;
-        case KC_COLEMAK:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_COLEMAK);
-            }
-            return false; 
+        // case KC_QWERTY:
+        //     if (record->event.pressed) {
+        //         set_single_persistent_default_layer(_QWERTY);
+        //     }
+        //     return false;
+        // case KC_COLEMAK:
+        //     if (record->event.pressed) {
+        //         set_single_persistent_default_layer(_COLEMAK);
+        //     }
+        //     return false; 
         case KC_COLEMAKDH:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_COLEMAKDH);
