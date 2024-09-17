9cc3d13f933cf33f1aa2e4f814545b109a52bc7d
Dane Evans <dane@2pisoftware.com>
2024-09-18 00:38:59 +1000

    remove all but dh

--- keyboards/gecko/keymaps/devdev/keymap.c
+++ keyboards/gecko/keymaps/devdev/keymap.c
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// layer switcher 
 	[_SWITCH] = LAYOUT_split_3x6_5(\
 	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
-	      TO(_DEFAULTS),TO(_LOWER),TO(_RAISE),TO(_ADJUST),TO(_NUMPAD),KC_NO, 	    KC_NO,   TO(7),   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+	      TO(_COLEMAKDH),TO(_LOWER),TO(_RAISE),TO(_ADJUST),TO(_NUMPAD),KC_NO, 	    KC_NO,   TO(7),   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 	      KC_NO,   KC_NO, KC_BRIU,   KC_NO,   KC_NO,   KC_NO, 						 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, \
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
