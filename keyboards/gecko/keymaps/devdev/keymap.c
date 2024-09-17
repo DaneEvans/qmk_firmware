69c744ec7911c3146bc9f6c06127204c5fc8ab19
Dane Evans <dane@2pisoftware.com>
2024-09-17 23:52:44 +1000

    fix c line endings

--- keyboards/gecko/keymaps/devdev/keymap.c
+++ keyboards/gecko/keymaps/devdev/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),	  
 
 	// layer switcher 
-	[_SWITCH] = LAYOUT(\
+	[_SWITCH] = LAYOUT_split_3x6_5(\
 	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
 	      TO(_DEFAULTS),TO(_LOWER),TO(_RAISE),TO(_ADJUST),TO(_NUMPAD),KC_NO, 	    KC_NO,   TO(7),   KC_NO,   KC_NO,   KC_NO,   RESET,  \
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
